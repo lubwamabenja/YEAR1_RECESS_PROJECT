7c35bd9ba85af17a0c6ba2b3e095d3121c13ecf8
Schwartzel <mkaluuma93@gmail.com>
2019-08-01 09:21:39 +0300

    the branch contain aunthenticatin file

---
+++ database/migrations/2019_06_15_100018_create_districts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDistrictsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('districts', function (Blueprint $table) { 
+            $table->increments('id');
+            $table->string('districtName',30);
+            $table->string('headAgent',30);
+            $table->integer('no_of_members');
+            $table->integer('no_of_agents'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('districts');
+    }
+}
